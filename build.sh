f08e39b499b86dcca6675eeb745cbdeb8374bee8
Lake <lake.hopkins@outlook.com>
2025-09-07 20:23:55 +1000

    tweaked project layout in preperation for rewrite

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-make clean package
+cp -r . /usr/plugins/net-mgmt/os-oidc
 
+cd /usr/plugins/net-mgmt/os-oidc
+make clean package
 pkg delete -fy os-oidc*
 pkg add $(find work/pkg/*.pkg)
