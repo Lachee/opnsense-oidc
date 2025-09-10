4c923f8d232e4982a12fdc5be85797b4604ef3ac
lachee <lachlan.h@hotmail.com>
2025-09-10 11:37:59 +1000

    Fixed property access for objects

--- src/opnsense/mvc/app/library/OPNsense/Auth/SSOProviders/OIDCContainer.php
+++ src/opnsense/mvc/app/library/OPNsense/Auth/SSOProviders/OIDCContainer.php
@@ -17,19 +17,19 @@ class OIDCContainer implements ISSOContainer
             $authServers = [$authServers];
 
         foreach ($authServers as $server) {
-            if ($server['type'] !== 'oidc')
+            if ($server->type !== 'oidc')
                 continue;
 
             $opts = [
                 'service' => 'WebGui',
-                'name' => $server['name'],
-                'login_uri' => "/api/oidc/auth/login?provider={$server['name']}",
+                'name' => $server->name,
+                'login_uri' => "/api/oidc/auth/login?provider={$server->name}",
             ];
 
-            if (!empty($server['oidc_custom_button'])) {
-                $opts['html_content'] = $server['oidc_custom_button'];
+            if (!empty($server->oidc_custom_button)) {
+                $opts['html_content'] = $server->oidc_custom_button;
 
-                $iconUrl = "/api/oidc/auth/icon?provider={$server['name']}";
+                $iconUrl = "/api/oidc/auth/icon?provider={$server->name}";
                 $opts['html_content'] = str_replace('%icon%', $iconUrl, $opts['html_content']);
                 $opts['html_content'] = str_replace('%name%', $opts['name'], $opts['html_content']);
                 $opts['html_content'] = str_replace('%url%', $opts['login_uri'], $opts['html_content']);
