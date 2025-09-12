053751f8d97669e582b803ffbc43cd783268cf1f
Lachee <lachlan.h@hotmail.com>
2025-09-12 11:37:08 +1000

    cleaned up user creation

--- src/opnsense/mvc/app/controllers/OPNsense/Oidc/Api/AuthController.php
+++ src/opnsense/mvc/app/controllers/OPNsense/Oidc/Api/AuthController.php
@@ -2,11 +2,16 @@
 
 namespace OPNsense\Oidc\Api;
 
+use InvalidArgumentException;
 use OPNsense\Auth\AuthenticationFactory;
 use OPNsense\Auth\OIDC;
+use OPNsense\Auth\User;
 use OPNsense\Base\ApiControllerBase;
+use OPNsense\Base\FieldTypes\ArrayField;
+use OPNsense\Core\Backend;
 use OPNsense\Core\Config;
 use OPNsense\Oidc\OidcClient;
+use RuntimeException;
 
 /**
  * Class ServiceController
@@ -129,7 +134,7 @@ class AuthController extends ApiControllerBase
             }
 
             // Create the user if allowed
-            $localUser = $this->createLocalUser($lookupUsername, $lookupEmail);
+            $localUser = $this->createLocalUser($lookupUsername, $lookupEmail, $user->name ?? '', []);
             if ($localUser === false) {
                 $this->response->setStatusCode(500, "User creation failed");
                 return "Unable to create local account.";
@@ -195,31 +200,38 @@ class AuthController extends ApiControllerBase
         return false;
     }
 
+
     /** Creates a new local user. */
-    protected function createLocalUser($username, $email)
+    protected function createLocalUser($username, $email, $displayName = '', $groups = [])
     {
         if (!self::ALLOW_USER_CREATION)
-            throw new \RuntimeException('Cannot create new users');
+            return false;
 
-        // The following code was thrown together and not properly tested.
-        throw new \RuntimeException('Not Yet Implemented / Tested.');
+        if (empty($username)) 
+            return false;
 
-        $cnf = Config::getInstance()->object();
-        $user = $cnf->system->user->addChild('user');
-        $user->name     = $username;
-        $user->descr    = "OIDC User";
-        $user->scope    = "user";
-        $user->disabled = "0";
-        if (!empty($email)) {
-            $user->email = $email;
+        $mdl = new User();
+        
+        /** @var ArrayField $users */
+        $users = $mdl->user;
+        $node = $users->add();
+        $node->name     = $username;
+        $node->email    = $email ?? '';
+        $node->descr    = $displayName ?? $username;
+        $node->comment  = "Created with OpenID Connect";
+        $node->password = 'DEADBEEF';
+        $node->scrambled_password = "1";
+        $node->scope    = "user";
+        $node->disabled = "0";
+        
+        // Serialize and Save
+        if (!$mdl->serializeToConfig()) {
+            // TODO: Log failed
+            return false;
         }
-        // no password set, authentication handled via OIDC
 
         Config::getInstance()->save();
-        return [
-            'name'   => $username,
-            'email'  => $email,
-            'groups' => []
-        ];
+        (new Backend())->configdpRun('auth sync user', [$node->name]);
+        return ['name' => $node->name];
     }
 }
